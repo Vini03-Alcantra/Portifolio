d4e16e901e8b53e57e07873194574d2f01795367
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-21 23:07:13 -0300

    Incluindo preço do layout

--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,14 @@ document.querySelector(".hamburguer").addEventListener("click", () => {
 })
 
 document.querySelector("#qtde").addEventListener("change", atualizaPreco);
-
-function atualizaPreco(){
+document.querySelector("#js").addEventListener("change", atualizaPreco);
+document.querySelector("#layout-sim").addEventListener("change", atualizaPreco)
+function atualizaPreco(){    
     const qtde = document.querySelector("#qtde").value;
     const temJS = document.querySelector("#js").checked;
+    const layout = document.querySelector("#layout-sim").checked;
     let preco = qtde * 100;
+    if(layout) preco = preco + 500;
     if (temJS) preco += (preco/10)
     document.querySelector("#preco").innerHTML = `R$ ${preco}`;
 }
