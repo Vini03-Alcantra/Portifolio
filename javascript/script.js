acee1383d1310d60e83f3f7dbb974a26ce0badb8
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-21 21:58:55 -0300

    valor sento modificado

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,11 @@
 document.querySelector(".hamburguer").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("show-menu");
 })
+
+document.querySelector("#qtde").addEventListener("change", atualizaPreco);
+
+function atualizaPreco(){
+    const qtde = document.querySelector("#qtde").value;
+    let preco = qtde * 100;
+    document.querySelector("#preco").innerHTML = `R$ ${preco}`;
+}
