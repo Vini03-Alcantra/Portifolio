d65dc16e6ff538e332bcc015e742874a8d426766
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-21 22:05:10 -0300

    Calculando 10% quando incluindo JS

--- javascript/script.js
+++ javascript/script.js
@@ -6,6 +6,8 @@ document.querySelector("#qtde").addEventListener("change", atualizaPreco);
 
 function atualizaPreco(){
     const qtde = document.querySelector("#qtde").value;
+    const temJS = document.querySelector("#js").checked;
     let preco = qtde * 100;
+    if (temJS) preco += (preco/10)
     document.querySelector("#preco").innerHTML = `R$ ${preco}`;
 }
