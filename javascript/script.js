99fe3311528373c69df2a4a1b6e3167573f1fb92
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-21 23:38:23 -0300

    Incluindo preço da antecipação

--- javascript/script.js
+++ javascript/script.js
@@ -5,12 +5,22 @@ document.querySelector(".hamburguer").addEventListener("click", () => {
 document.querySelector("#qtde").addEventListener("change", atualizaPreco);
 document.querySelector("#js").addEventListener("change", atualizaPreco);
 document.querySelector("#layout-sim").addEventListener("change", atualizaPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizaPreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value;
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`;
+    atualizaPreco()
+})
 function atualizaPreco(){    
     const qtde = document.querySelector("#qtde").value;
     const temJS = document.querySelector("#js").checked;
-    const layout = document.querySelector("#layout-sim").checked;
+    const layout = document.querySelector("#layout-sim").checked;    
+    const prazo = document.querySelector("#prazo").value;
+
     let preco = qtde * 100;
-    if(layout) preco = preco + 500;
     if (temJS) preco += (preco/10)
-    document.querySelector("#preco").innerHTML = `R$ ${preco}`;
+    if(layout) preco = preco + 500;
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia;
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
 }
